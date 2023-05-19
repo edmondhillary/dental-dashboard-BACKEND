c96746b289039d253f523d42cf7425730b16d5c4
Eduardo Gonzalez <edu14937@gmail.com>
2023-05-19 16:30:56 +0200

    the last commit

--- src/api/models/employeeSchema.js
+++ src/api/models/employeeSchema.js
@@ -1,103 +1,97 @@
 import { Schema, model } from "mongoose";
 
-const employeeSchema = new Schema({
-  treatments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Treatment",
-    },
-  ],
-  password: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-
-  },
-  lastName: {
-    type: String,
-
-    
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-
-    
-  },
-  dateOfBirth: {
-    type: Date,
-    
-  },
-
-  gender: {
-    type: String,
-    
-  },
-  address: {
-    type: String,
-  
-    
-  },
-  speciality: {
-    type: String,
-    
-  },
-  role: {
-    type: String,
-    default: "Employee",
-  },
-  lastConnection: {
-    type: Date,
-    default: Date
-  },
-  securityNumber: {
-    type: Number,
-    
-  },
-  dni: {
-    type: String,
-    
-  },
-  patients: [
+const employeeSchema = new Schema(
   {
-    type: Schema.Types.ObjectId,
-    ref: 'Patient'
-  }],
-  appointments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Appointment'
-    }],
-  budgets: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Budget'
-    }
-  ],
-  displayName: {
-    type: String,
-    get: function () {
-      return this.firstName + " " + this.lastName;
+    treatments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Treatment",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+
+    gender: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    speciality: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "Employee",
+    },
+    lastConnection: {
+      type: Date,
+      default: Date,
+    },
+    securityNumber: {
+      type: Number,
+    },
+    dni: {
+      type: String,
+    },
+    patients: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Patient",
+      },
+    ],
+    appointments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Appointment",
+      },
+    ],
+    budgets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Budget",
+      },
+    ],
+    displayName: {
+      type: String,
+      get: function () {
+        return this?.firstName + " " + this?.lastName;
+      },
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://www.shutterstock.com/image-illustration/vector-medical-icon-dentist-doctor-260nw-1492048499.jpg",
     },
   },
-  avatar: {
-    type: String,
-    default: "https://www.shutterstock.com/image-illustration/vector-medical-icon-dentist-doctor-260nw-1492048499.jpg"
+  {
+    timestamps: true,
+    toObject: { getters: true },
+    toJSON: { getters: true, versionKey: false },
   }
-  
-}, { timestamps: true,
-  toObject: { getters: true },
-  toJSON: { getters: true , versionKey: false}
-});
+);
 
-
-
-const employeeModel = model('Employee', employeeSchema);
+const employeeModel = model("Employee", employeeSchema);
 
 export default employeeModel;
 
