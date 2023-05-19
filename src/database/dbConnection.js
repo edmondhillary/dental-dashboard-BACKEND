7b1def6372d293308df129c40a17db66a9c2c493
Eduardo Gonzalez <edu14937@gmail.com>
2023-05-19 13:42:40 +0200

    sera el final?

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -3,14 +3,15 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const { MONGO_DB_NAME, MONGO_URL_DENTAL_CLINIC} = process.env;
+MONGO_DB_NAME = "dentalClinic"
+const MONGO_URL_DENTAL_CLINIC = 'mongodb+srv://root:rootroot@cluster0.5qgofue.mongodb.net/?retryWrites=true&w=majority'; // Reemplaza <password> con la contraseña correcta
 const connectionConfig = { dbName: MONGO_DB_NAME, autoIndex: true };
 const connection = await mongoose.connect(MONGO_URL_DENTAL_CLINIC, connectionConfig);
 
 if (connection) {
-  console.log('CONNECTION with mongo database successfully ');
+  console.log('CONNECTION with MongoDB database successfully');
 } else {
-  console.error('Error to connect with mongo database');
+  console.error('Error connecting to MongoDB database');
 }
 
-// https://www.w3schools.com/nodejs/nodejs_mysql.asp
+// Resto del código...
